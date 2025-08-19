e458cd1636d530a16c43fadd3831aa5139733e7e
xshotss <adab14222@gmail.com>
2025-08-19 13:52:40 +0300

    Improved library documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
 /*!
-* rs-audio is a Rust library for making retro music programmatically.<br>
-  Currently, it has support for: <br>Sine waves,<br>Squares,<br>Sawtooths,<br>and Triangles.<br><br>
-# Usage:<br>
+# RS-AUDIO
+rs-audio is a Rust library for making retro music programmatically.<br>
+  Currently, it has support for: 
+  * Sine waves,
+  * Squares,
+  * Sawtooths,
+  * and Triangles.<br>
+
+This library is MIT licensed. <br>Learn more in our repository: <https://github.com/xshotss/rs-audio/blob/main/LICENSE><br><br>
+## Usage:<br>
 
 To create a default song (to make sure everything is working):
 ```
